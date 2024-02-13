fd4f99f97696d238ea5f99c6c1013543ccf9252a
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-02-13 10:04:47 +0200

    Nested Query

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const resolvers = {
         games() {
             return db.games
         },
-        game(_, { id }) {
+        game(_, { id }) { //args as {id} 
             return db.games.find((g) => g.id === id);
         },
         reviews() {
             return db.reviews
         },
-        review(_, { id }) {
+        review(_, { id }){
             return db.reviews.find((r) => r.id === id);
         },
         authors() {
@@ -26,6 +26,24 @@ const resolvers = {
         },
         author(_, { id }) {
             return db.authors.find((a) => a.id === id);
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent){
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
+    Review: {
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
+        },
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
         }
     }
 }
